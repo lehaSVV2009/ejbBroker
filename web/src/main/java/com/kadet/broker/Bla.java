10443e524fc2d14f03c49df5514eb9b840c391d9
lehaSVV2009 <lehaSVV2009@gmail.com>
2014-09-29 03:34:41 +0300

    Maven JSF EJB example

---
+++ web/src/main/java/com/kadet/broker/Bla.java
@@ -0,0 +1,18 @@
+package com.kadet.broker;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Date: 29.09.14
+ * Time: 3:32
+ *
+ * @author SarokaA
+ */
+@ManagedBean
+@SessionScoped
+public class Bla {
+
+
+
+}
